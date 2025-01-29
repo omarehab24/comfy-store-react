2861634ba3741b03e918cb77946d0b057a6638ab
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-29 04:11:55 +0200

    Add custom css to equally align components in HomeLayout

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -7,7 +7,9 @@ const HomeLayout = () => {
        <nav>
         <span className='text-4xl text-primary'>Comfy</span>
        </nav>
+       <section className='align-element py-20'>
        <Outlet />
+       </section>
      </>
     );
   };
