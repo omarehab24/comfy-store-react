7884b25e3f0fba7240c616a8c2e77cc3a7e50cf5
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-29 03:58:45 +0200

    Add form input, submit button & styling for register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,21 @@
+import { Link, Form } from "react-router-dom";
+import { SubmitButton, FormInput } from "../components";
+
 const Register = () => {
-    return (
-      <div>
-        <h1>Register</h1>
+  return <section className="h-screen grid place-items-center">
+    <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+      <h4 className="text-center font-bold text-3xl">Register</h4>
+      <FormInput type="text" label="username" name="username" />
+      <FormInput type="email" label="email" name="email" />
+      <FormInput type="password" label="password" name="password" />
+      <div className="mt-4">
+        <SubmitButton text="Register" />
       </div>
-    );
-  };
-  
-  export default Register;
+      <p className="text-center">
+        Already have an account? <Link to="/login" className="ml-2 link link-hover link-primary capitalize">login</Link>
+      </p>
+    </Form>
+  </section>
+};
+
+export default Register;
