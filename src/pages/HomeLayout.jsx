8c6b436ad8d0c9230c185d900359d8ec5fe65f21
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-30 07:36:17 +0200

    Add header component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,16 @@
 // Outlet: Renders the matching child route of a parent route or nothing if no child route matches. 
 import { Outlet } from 'react-router-dom';
+import { Header } from '../components';
 
 const HomeLayout = () => {
-    return (
-     <>
-       <nav>
-        <span className='text-4xl text-primary'>Comfy</span>
-       </nav>
-       <section className='align-element py-20'>
-       <Outlet />
-       </section>
-     </>
-    );
-  };
-  
-  export default HomeLayout;
+  return (
+    <>
+      <Header />
+      <section className='align-element py-20'>
+        <Outlet />
+      </section>
+    </>
+  );
+};
+
+export default HomeLayout;
