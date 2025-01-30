8e70deda11655e2981ae6bfba27081148229d93f
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-30 09:16:22 +0200

    Add navbar component with styling and links

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
 // Outlet: Renders the matching child route of a parent route or nothing if no child route matches. 
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components';
+import { Header, NavBar } from '../components';
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <NavBar />
       <section className='align-element py-20'>
         <Outlet />
       </section>
