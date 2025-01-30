f5a3b5c5ffef24ba6fd6f077112b0baed1d09fd6
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-30 09:33:57 +0200

    Add navlinks component for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs'
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom'
+import NavLinks from './NavLinks'
 
 function NavBar() {
     return (
-        <nav className='bg-base-200'>
+        <nav className='bg-base-300'>
             <div className='navbar align-element'>
                 <div className='navbar-start'>
                     {/* TITLE */}
@@ -14,13 +15,15 @@ function NavBar() {
                         <label tabIndex={0} className='btn btn-ghost lg:hidden'>
                             <FaBarsStaggered className='h-6 w-6' />
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
-                            links
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-300 rounded-box w-52">
+                            <NavLinks />
                         </ul>
                     </div>
                 </div>
                 <div className='navbar-center hidden lg:flex'>
-                    <ul className='menu menu-horizontal'>links</ul>
+                    <ul className='menu menu-horizontal'>
+                        <NavLinks />
+                    </ul>
                 </div>
                 <div className='navbar-end'>
                     {/* THEME */}
