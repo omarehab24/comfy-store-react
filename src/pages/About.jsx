8299d4faf19a6ceb5796bb007b9729220b5c7b81
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-30 10:58:42 +0200

    Add text and styling for the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,16 @@
 const About = () => {
-    return <h1>About</h1>;
-  };
+  return <>
+    <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+      <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">We love</h1>
+      <div className="stats bg-primary shadow">
+        <div className="stat">
+          <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">Comfy</div>
+        </div>
+      </div>
+    </div>
+    <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At quibusdam facere magni! Ullam, ipsum dignissimos? Eos autem id fuga in incidunt debitis, voluptatum, accusamus, unde quas quia totam? Officiis cupiditate tempora quo cumque? Nemo esse officia, quos odio provident fugit labore optio, officiis magnam possimus minus quia illum, eaque tempore. Iure provident consequuntur porro aperiam perspiciatis cum vitae nihil beatae.
+    </p>
+  </>
+};
 export default About;
