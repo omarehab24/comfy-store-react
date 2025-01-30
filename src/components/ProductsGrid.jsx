5afb4252b1514feebd5b3f64109b97ef8bfb3c50
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-01-30 22:23:11 +0200

    Add price formatting to USD functionality

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from "react-router-dom"
+import { formatPrice } from "../utils"
 
 function ProductsGrid() {
     const { products } = useLoaderData()
@@ -8,13 +9,14 @@ function ProductsGrid() {
             {
                 products.map((product) => {
                     const { title, image, price } = product.attributes
+                    const priceUSD = formatPrice(price)
                     return <Link to={`/products/${product.id}`} key={product.id} className="card w-full shadow-xl hover:shadow-2xl transition duration-300">
                         <figure className="px-4 pt-4">
                             <img src={image} alt={title} className="rounded-xl h-64 md:h-48 w-full object-cover" />
                         </figure>
                         <div className="card-body items-center text-center">
                             <h2 className="card-title capitalize tracking-wider">{title}</h2>
-                            <span className="text-secondary">{price}</span>
+                            <span className="text-secondary">{priceUSD}</span>
                         </div>
                     </Link>
                 })
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,10 @@ const productionUrl = 'https://strapi-store-server.onrender.com/api'
 export const customFetch = axios.create({
     baseURL: productionUrl
 })
+
+export const formatPrice = (price) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format((price / 100).toFixed(2))
+}
