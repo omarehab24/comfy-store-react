41189060d552bfbb0fc2394bc12be76ac7be6c32
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-01 09:37:21 +0200

    Fetch single product data from the backend and add text and styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import {
 import { ErrorElement } from './components';
 
 import { loader as landingLoader } from './pages/Landing'
+import { loader as singleProductLoader } from './pages/SingleProduct'
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,8 @@ const router = createBrowserRouter([
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader
       },
       {
         path: 'cart',
