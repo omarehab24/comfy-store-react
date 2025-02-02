e946b31f415b6bb6db12e524b07552f65e326a7b
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-02 10:19:57 +0200

    Add FormSelect component for selecting inputs in filters component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,19 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 function Filters() {
+    const { meta } = useLoaderData()
     return (
         <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
             {/* SEARCH */}
             <FormInput type='search' label='Search Products' name='search' size='input-sm' />
+            {/* CATEGORIES */}
+            <FormSelect type='select' label='Select Category' name='category' size='select-sm' list={meta.categories} />
+            {/* COMPANIES */}
+            <FormSelect type='select' label='Select Company' name='company' size='select-sm' list={meta.companies} />
+            {/* ORDER */}
+            <FormSelect type='select' label='Select Order' name='order' size='select-sm' list={['name-a-z', 'name-z-a', 'price-low-to-high', 'price-high-to-low']} />
             {/* BUTTONS */}
             <button className='btn btn-primary btn-sm' type='submit'>
                 Search
---
+++ src/components/FormSelect.jsx
@@ -0,0 +1,17 @@
+function FormSelect({ label, name, list, defaultValue, size }) {
+
+    return (
+        <div className="form-control">
+            <label htmlFor={name} className="label">
+                <span className="label-text capitalize">{label}</span>
+            </label>
+            <select name={name} defaultValue={defaultValue} className={`select select-bordered ${size}`} id={name}>
+                {list.map((item) => (
+                    <option key={item} value={item}>{item}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default FormSelect
