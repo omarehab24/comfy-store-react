a945d5ac2e6231fe5cf0b5d9f75c671326ee9f88
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-03 10:50:46 +0200

    Add clearCart, removeItem & editItem reducers in cartSlice for manipulating product in cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -21,7 +21,7 @@ const cartSlice = createSlice({
         addItem: (state, action) => {
             const { product } = action.payload;
             const item = state.cartItems.find((item) => item.cartID === product.cartID);
-            
+
             if (item) {
                 item.amount += product.amount;
             } else {
@@ -32,9 +32,36 @@ const cartSlice = createSlice({
             cartSlice.caseReducers.calculateCartTotal(state);
             toast.success('Item added to cart!');
         },
-        clearCart: (state) => {},
-        removeItem: (state, action) => {},
-        editItem: (state, action) => {},
+        clearCart: () => {
+            localStorage.setItem('cart', JSON.stringify(defaultState));
+            return defaultState
+        },
+        removeItem: (state, action) => {
+            const { cartID } = action.payload;
+            const product = state.cartItems.find((item) => item.cartID === cartID);
+            state.cartItems = state.cartItems.filter((item) => item.cartID !== cartID);
+            state.numItemsInCart -= product.amount;
+            state.cartTotal -= product.price * product.amount;
+            cartSlice.caseReducers.calculateCartTotal(state);
+            toast.error('Item removed from cart!'); // chosen to use toast.error just for the color (red) not an actual error
+        },
+        editItem: (state, action) => {
+            const { cartID, amount } = action.payload;
+            const product = state.cartItems.find((item) => item.cartID === cartID);
+            // amount is the new quantity the user wants
+            // product.amount is the current quantity in the cart
+            // amount - product.amount gives us the difference
+            // e.g.
+            // If changing from 2 items to 5: 5 - 2 = +3 (adding 3 to total)
+            // If changing from 5 items to 2: 2 - 5 = -3 (subtracting 3 from total)
+            state.numItemsInCart += amount - product.amount;
+            // e.g.
+            // Changing from 2 to 5: $10 * (5 - 2) = $10 * 3 = +$30
+            state.cartTotal += product.price * (amount - product.amount);
+            product.amount = amount;
+            cartSlice.caseReducers.calculateCartTotal(state);
+            toast.success('Cart updated!');
+        },
         calculateCartTotal: (state) => {
             state.tax = state.cartTotal * 0.1;
             state.orderTotal = state.cartTotal + state.tax + state.shipping;
