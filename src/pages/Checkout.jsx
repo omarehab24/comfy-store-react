e277aa33220fda93deaf3729ae8f56e6f716015f
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-05 15:26:22 +0200

    Restrict access to checkout page if user isn't logged in

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,16 @@
 import { useSelector } from 'react-redux';
 import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+export const loader = (store) => () => {
+  const user = store.getState().userState.user;
+  if (!user) {
+    toast.warn('You must be logged in to place an order');
+    return redirect('/login');
+  }
+  return null
+}
 
 const Checkout = () => {
   const cartTotal = useSelector(state => state.cartState.cartTotal);
