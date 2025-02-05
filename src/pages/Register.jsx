66dd17c4d58195dac577c3ab9f0a8173aeacb198
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-05 11:31:56 +0200

    Add register functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,21 @@
-import { Link, Form } from "react-router-dom";
+import { Link, Form, redirect } from "react-router-dom";
 import { SubmitButton, FormInput } from "../components";
+import { toast } from "react-toastify";
+import { customFetch } from "../utils";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/local/register", data)
+    toast.success("Registered successfully!");
+    return redirect("/login");
+  } catch (error) {
+    const errorMessage = error?.response?.data?.error?.message || "Something went wrong";
+    toast.error(errorMessage);
+    return null;
+  }
+};
 
 const Register = () => {
   return <section className="h-screen grid place-items-center">
