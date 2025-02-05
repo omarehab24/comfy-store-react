122d8ca27e453ea2376ca8cbbc08f8adbafc9352
omarehab24 <89408282+omarehab24@users.noreply.github.com>
2025-02-05 14:47:59 +0200

    Add login functionality as guest user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,15 +21,29 @@ export const action = (store) => async ({ request }) => {
 }
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const loginAsGuest = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", { identifier: "test@test.com", password: "secret" });
+      dispatch(login(response.data));
+      navigate("/");
+    } catch (error) {
+      const errorMessage = error?.response?.data?.error?.message || "Something went wrong";
+      toast.error(errorMessage);
+      return null;
+    }
+  }
   return <section className="h-screen grid place-items-center">
     <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
       <h4 className="text-center font-bold text-3xl">Login</h4>
-      <FormInput type="email" label="email" name="identifier" defaultValue="test@test.com" />
-      <FormInput type="password" label="password" name="password" defaultValue="secret" />
+      <FormInput type="email" label="email" name="identifier" />
+      <FormInput type="password" label="password" name="password" />
       <div className="mt-4">
         <SubmitButton text="Login" />
       </div>
-      <button type="button" className="btn btn-secondary btn-block">Guest User</button>
+      <button type="button" className="btn btn-secondary btn-block" onClick={loginAsGuest}>Guest User</button>
       <p className="text-center">
         Don&apos;t have an account? <Link to="/register" className="ml-2 link link-hover link-primary capitalize">register</Link>
       </p>
