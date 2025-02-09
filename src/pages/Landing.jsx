541278fcb52ea5f75f7e597455e71a64501c0f11
Omar Ehab <89408282+omarehab24@users.noreply.github.com>
2025-02-09 10:51:00 +0200

    Refactor landing page's loader to leverage react query's caching capability

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,8 +4,16 @@ import { FeaturedProducts } from '../components'
 
 const url = '/products?featured=true';
 
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url)
+}
+
 export const loader = (queryClient) => async () => {
-  const response = await customFetch(url);
+  // ensureQueryData() is a React Query method that checks if data for a query already exists in the cache, and if it doesn't, it fetches it.
+  // If the data exists and is still fresh, it returns that cached data.
+  // If the data doesn't exist or is stale, it executes the queryFn to fetch new data.
+  const response = await queryClient.ensureQueryData(featuredProductsQuery);
   const products = response.data.data
   return { products }
 };
